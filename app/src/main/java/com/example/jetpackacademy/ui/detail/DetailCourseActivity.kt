daecdd281e90e1d0c3c67d9596924cd68ee5a546
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-12-16 10:58:54 +0700

    layouts

--- app/src/main/java/com/example/jetpackacademy/ui/detail/DetailCourseActivity.kt
+++ app/src/main/java/com/example/jetpackacademy/ui/detail/DetailCourseActivity.kt
@@ -5,8 +5,12 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.jetpackacademy.R
+import com.example.jetpackacademy.databinding.ActivityDetailCourseBinding
+import com.example.jetpackacademy.databinding.ContentDetailCourseBinding
 
 class DetailCourseActivity : AppCompatActivity() {
+
+    private lateinit var detailContentBinding: ContentDetailCourseBinding
 
     companion object {
         const val EXTRA_COURSE = "extra_course"
@@ -14,14 +18,12 @@ class DetailCourseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_course)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
+        val activityDetailCourseBinding = ActivityDetailCourseBinding.inflate(layoutInflater)
+        detailContentBinding = activityDetailCourseBinding.detailContent
+        setContentView(detailContentBinding.root)
+
+        setSupportActionBar(activityDetailCourseBinding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
