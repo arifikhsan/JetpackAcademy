66e935263faca024b77292027daa5794ee7c1102
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-12-16 10:34:09 +0700

    files

---
+++ app/src/main/java/com/example/jetpackacademy/data/ContentEntity.kt
@@ -0,0 +1,5 @@
+package com.example.jetpackacademy.data
+
+data class ContentEntity(
+    var content: String?
+)
--- app/src/main/java/com/example/jetpackacademy/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/jetpackacademy/ui/home/HomeActivity.kt
@@ -1,7 +1,8 @@
-package com.example.jetpackacademy
+package com.example.jetpackacademy.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.jetpackacademy.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
